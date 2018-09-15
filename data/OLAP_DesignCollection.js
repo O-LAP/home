7cc1e497894e02498265c1308d5a6d55aca40380
Amit Nambiar <amit.lzkpa@gmail.com>
2018-09-15 01:18:48 -0400

    Added mendel

--- data/OLAP_DesignCollection.js
+++ data/OLAP_DesignCollection.js
@@ -3,7 +3,8 @@
 
 var OLAP_DesignCollection =  [
 	"https://github.com/amitlzkpa/o-lap_plato",
-	"https://github.com/O-LAP/o-lap_fatplato"
+	"https://github.com/O-LAP/o-lap_fatplato",
+	"https://github.com/amitlzkpa/mendel"
 	];
 
 	
