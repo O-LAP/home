c8e0d87ba10e1c87214a70270ccb3d782d9630f3
Amit Nambiar <amit.lzkpa@gmail.com>
2018-10-18 04:14:43 -0400

    Update OLAP_DesignCollection.js

--- data/OLAP_DesignCollection.js
+++ data/OLAP_DesignCollection.js
@@ -3,7 +3,8 @@
 
 var OLAP_DesignCollection =  [
 	"https://github.com/amitlzkpa/o-lap_plato",
-	"https://github.com/amitlzkpa/mendel"
+	"https://github.com/amitlzkpa/mendel",
+	"https://github.com/amitlzkpa/hobbes_bed"
 	];
 
 	
